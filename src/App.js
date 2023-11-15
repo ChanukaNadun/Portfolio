bb5f81cb0bd6128e9210d1bb0df24f1235965ddb
ChanukaNadun <chanuka@liveroom.dev>
2023-11-15 16:32:02 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './index.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
+      </head>
+        <div class="wrapper">
+            <nav class="navbar">
+                <div class="logo">
+                    <span>Logo</span>
+                </div>
+                <div class="nav">
+                  <div class="nav-menu" id="navMenu0">
+                    <ul class="link_list">
+                      <div class="close_menu">
+                        <i class="fa fa-times" onClick="menuClose();"></i>
+                      </div>
+                        <li class="list">
+                          <a href="#" class='link'>HOME</a>
+                        </li>
+                        <li class="list">
+                          <a href="#" class='link'>SERVICES</a>
+                        </li>
+                        <li class="list">
+                          <a href="#" class='link'>BLOG</a>
+                        </li>
+                        <li class="list">
+                          <a href="#" class='link'>ABOUT</a>
+                        </li>
+                        <li class="list">
+                          <a href="#" class='link'>CONTACT</a>
+                        </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <a href ="#" class="btn"><i class="fa fa-download"></i><span>Download CV</span></a>
+                  </div>
+                  <div class="mode">
+                    <div class="moon-sun" id="toggle-switch">
+                      <i class ="fa fa-moon" id="moon"></i>
+                      <i class="fa fa-sun" id="sun"></i>
+                    </div>
+                    <div class="nav_menu_btn">
+                      <i class="fa fa-bars" onclick="myMenuFunction();"></i>
+                    </div>
+                  </div>
+                </div>
+            </nav>
+            <main>
+              <div class="col col-1">
+                <div class="hero_box">
+                  <div class="hello">
+                    <p>Hello I'm</p>
+                  </div>
+                  <div class="headline">
+                    <span class="multiText"></span>
+                  </div>
+                  <div class="hello">
+                    <p>My name is CHANUKA NADUN, I'm web developer with over 1 year experience.</p>
+                  </div>
+                  <div class="social_icons">
+                    <div class="icon_circle"></div>
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#"  class="fa fa-linkedin-square"></a>
+                    <a href="#" class="fa fa-whatsapp"></a>
+                  </div>
+                  <div class="hero_btn">
+                    <button class="btn_1">Hire me</button>
+                    <button class="btn_2"><a href="#">Download CV</a></button>
+                  </div>
+                </div>
+              </div>
+            </main>
+            <div class="images">
+              <img src="" alt="" class="bg"></img>
+              <img src="" alt="" class="boy"></img>
+            </div>
+        </div>
     </div>
   );
 }
